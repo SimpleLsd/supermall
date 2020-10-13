c142790029205ff1742c91605614f22fb814da9f
SimpleLsd_MAC <liusungti@163.com>
2020-10-13 09:30:59 +0800

    首页的商品tab切换

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="tab-control">
+    <div
+      v-for="(item, index) in tabItems"
+      :key="item.index"
+      class="tab-control-item"
+      :class="{ active: index === currentIndex }"
+      @click="itemClick(index)"
+    >
+      <span>{{ item }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabControl',
+  props: {
+    tabItems: {
+      type: Array,
+      default() {
+        return []
+      },
+    },
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index
+    },
+  },
+  data() {
+    return {
+      currentIndex: 0,
+    }
+  },
+}
+</script>
+
+<style scroped>
+.tab-control {
+  display: flex;
+  justify-content: space-around;
+  position: sticky;
+  top: 44px;
+  height: 44px;
+  line-height: 44px;
+  font-size: 14px;
+  background-color: #fff;
+}
+.tab-control span {
+  padding: 5px;
+}
+.active {
+  color: var(--color-tint);
+  border-bottom: 3px solid var(--color-tint);
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,14 +6,81 @@
     <Swiper :bannerItems="banners"></Swiper>
     <RecommendViews :recommends="recommends"></RecommendViews>
     <FeatureView></FeatureView>
+    <TabControl :tabItems="['流行', '新款', '精选']" />
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
 import Swiper from 'components/common/swiper/Swiper'
+
+import TabControl from 'components/content/tabControl/TabControl'
+
 import RecommendViews from 'views/home/childComps/RecommendViews'
 import FeatureView from 'views/home/childComps/FeatureView'
+
 import { getHomeMultidata } from 'network/home.js'
 
 export default {
@@ -24,6 +91,7 @@ export default {
     Swiper,
     RecommendViews,
     FeatureView,
+    TabControl,
   },
 
   data() {
@@ -45,5 +113,8 @@ export default {
 .home-nav {
   background-color: var(--color-tint);
   color: white;
+}
+li {
+  list-style: none;
 }
 </style>
